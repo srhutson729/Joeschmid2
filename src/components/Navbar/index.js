28fb5032af3e5cab4cd07c66cde5a87a736c064e
wambosa <shondiaz@gmail.com>
2021-02-13 20:50:56 +0000

    remove node modules and bring project into the root folder

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import { FaBars, Fatimes} from "react-icons/fa";
+import { IconContext } from "react-icons";
+import {Nav, NavbarContainer, NavLogo, MobileIcon} from "./NavbarElements";
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const [scroll, setScroll] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+            setScroll(true)
+        } else {
+            setScroll(false)
+        }
+    }
+
+    useEffect(() => {
+      changeNav()
+      window.addEventListener("scroll", changeNav)
+    }, [])
+
+ return (
+     <> 
+     <IconContext.Provider value={{ color: "#141414"}}>
+         <Nav active={scroll} click={click}>
+        <NavbarContainer>
+            <NavLogo to="/">
+                Joseph C Schmid
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+        </NavbarContainer>
+         </Nav>
+     </IconContext.Provider>
+     </>
+ )
+}
+
+export default NavBar
